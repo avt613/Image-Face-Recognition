484bc40a6f6f8843c6b5eb329a8b8a13be5cfead
avt613 <avrohomtravers@gmail.com>
2020-07-09 13:14:07 +0000

    initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+from cs50 import SQL
+db = SQL("sqlite:///data/faces.db")
+
+search = 'static/All/'
+extentions = ["JPG", "jpg", "JPEG", "jpeg", "BMP", "bmp", "PNG", "png"]
+tofinddir = "static/pics/"
+unknowndir = 'static/assets/img/'
+
+datadir = 'data/'
+knowndir = datadir + 'known/'
+known_face_encodings_loc = datadir + 'known_face_encodings.txt'
+known_face_names_loc = datadir + 'known_face_names.txt'
